e94e05494bfbf7f33258b4c064e791f440ad4c09
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-08-08 17:31:59 +0530

    feat(product list api )

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Sidebar from '../widgets/Sidebar'
+import ProductScreen from './ProductScreen'
 
 function DashboardScreen() {
     return (
         <>
             <Sidebar>
                 <Routes>
-                    <Route path="products" element={<h1>create</h1>} />
-                    <Route path='products/create' element={<h1>edit</h1>} />
+                    <Route path="products" element={<ProductScreen />} />
+                    <Route path='products/:id' element={<h1>edit</h1>} />
                 </Routes>
             </Sidebar>
         </>
---
+++ src/screens/ProductScreen.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import style from '../style/productScreen.module.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+function ProductScreen() {
+
+    const navigate = useNavigate();
+
+    const [response, setResponse] = useState([])
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/v1/product/')
+            .then(function (response) {
+                setResponse(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+            });
+    }, [])
+
+
+    return (
+        <div className={style.productScreen}>
+            {
+                response.map((item, index) =>
+                    <div className={style.productCard} key={index} onClick={() => navigate(`/dashboard/products/${item.id}`)}>
+                        <p>{item.ProductID}</p>
+                        <p>{item.ProductCode}</p>
+                        <p>{item.ProductName}</p>
+                        <p>{item.TotalStock}</p>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default ProductScreen
